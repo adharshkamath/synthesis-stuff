1dc45b2061b691992166aee2ac8f540d1d3dba07
Adharsh Kamath <adharshkamathr@gmail.com>
2022-06-30 16:12:54 +0530

    output formatting and tailrecursion

--- enumerative.ml
+++ enumerative.ml
@@ -1,4 +1,4 @@
-
+open List
 type exp =
     | Zero
     | One
@@ -28,28 +28,26 @@ let rec evaluate exp input = match exp with
   | Mult (a, b) -> evaluate a input * evaluate b input
   | IfNonNeg (a, b, c) -> if (evaluate a input > 0) then evaluate b input else evaluate c input
 
-let rec evaluateAll p inputs = List.map (evaluate p) inputs
+let evaluateAll p inputs = map (evaluate p) inputs
 
 let accountedFor pList p inputs =
   let pOutputs = evaluateAll p inputs in
-    let rec acc_helper pList = match pList with
-      | d::ds -> if pOutputs = (evaluateAll d inputs) then true else acc_helper ds
-      | [] -> false
-  in acc_helper pList
+  let helper d = (pOutputs = (evaluateAll d inputs)) in
+  exists helper pList
 
 let grow pList =
   let expansion = [] in
-  let nList = List.map (fun p -> Negate p) pList in
-  let plList = List.flatten (List.map (fun a -> List.map (fun b -> Plus (a, b)) pList) pList) in
-  let mlList = List.flatten (List.map (fun a -> List.map (fun b -> Mult (a, b)) pList) pList) in
-  let ifnList = List.flatten (List.map (fun x ->
-      List.flatten (List.map (fun a -> List.map (fun b -> IfNonNeg (x, a, b)) pList) pList)) pList) in
-  List.concat (expansion::nList::plList::mlList::ifnList::[pList])
+  let nList = map (fun p -> Negate p) pList in
+  let plList = flatten (map (fun a -> map (fun b -> Plus (a, b)) pList) pList) in
+  let mlList = flatten (map (fun a -> map (fun b -> Mult (a, b)) pList) pList) in
+  let ifnList = flatten (map (fun x ->
+      flatten (map (fun a -> map (fun b -> IfNonNeg (x, a, b)) pList) pList)) pList) in
+  concat (expansion::nList::plList::mlList::ifnList::[pList])
 
 let eliminateEquivalents pList inputs =
   let prunedList = [] in
   let rec elimeq_h pList prunedList = match pList with
-    | d::ds -> if accountedFor prunedList d inputs then elimeq_h ds prunedList else d::(elimeq_h ds prunedList)
+    | d::ds -> if accountedFor prunedList d inputs then elimeq_h ds prunedList else elimeq_h ds (d::prunedList)
     | [] -> prunedList in
   elimeq_h pList prunedList
 
@@ -59,8 +57,8 @@ let rec synth_h loops pList inputs outputs =
   if loops = 0 then raise TimeOut else
     begin
       let pList = eliminateEquivalents (grow pList) inputs in
-      let candList = List.filter (isCorrect inputs outputs) pList in
-      if candList = [] then synth_h (loops - 1) pList inputs outputs else List.hd candList
+      let candList = filter (isCorrect inputs outputs) pList in
+      if candList = [] then synth_h (loops - 1) pList inputs outputs else hd candList
     end
 
 let synthesize inputs outputs = synth_h 10 [Zero; One; In] inputs outputs
